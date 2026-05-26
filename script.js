18b1580c06665a63e450b302947e82fde1113f5b
Beksu1taan <beksultanjoker@gmail.com>
2026-05-26 17:52:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const elements = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  elements.forEach(el => {
+    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
+
+async function sendForm() {
+  const btn = document.querySelector(".form button");
+
+  const name = document.getElementById("name").value;
+  const course = document.getElementById("course").value;
+  const email = document.getElementById("email").value;
+
+  if (!name || !course || !email) {
+    alert("Заполни все поля");
+    return;
+  }
+
+  btn.innerText = "Sending...";
+  btn.disabled = true;
+
+  try {
+    const res = await fetch("https://dmd-backend-production.up.railway.app/subscribe", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ name, course, email })
+    });
+
+    const data = await res.json();
+
+    if (data.success) {
+      alert("Заявка отправлена");
+
+      document.getElementById("name").value = "";
+      document.getElementById("course").value = "";
+      document.getElementById("email").value = "";
+    } else {
+      alert("Ошибка при отправке");
+    }
+
+  } catch (err) {
+    alert("Сервер не отвечает");
+    console.log(err);
+  }
+
+  btn.innerText = "Subscribe";
+  btn.disabled = false;
+}
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  uri: process.env.MYSQL_URL
+});
+
+db.connect(err => {
+  if (err) {
+    console.log("DB ERROR:", err);
+  } else {
+    console.log("MySQL connected");
+
+    db.query(`
+      CREATE TABLE IF NOT EXISTS subscribers (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255),
+        course VARCHAR(255),
+        email VARCHAR(255)
+      )
+    `, (err) => {
+      if (err) {
+        console.log("TABLE ERROR:", err);
+      } else {
+        console.log("Table ready");
+      }
+    });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
+
+app.post("/subscribe", (req, res) => {
+  const { name, course, email } = req.body;
+
+  const sql = "INSERT INTO subscribers (name, course, email) VALUES (?, ?, ?)";
+
+  db.query(sql, [name, course, email], (err) => {
+    if (err) {
+      console.log("SQL ERROR:", err);
+      return res.json({ success: false });
+    }
+
+    res.json({ success: true });
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running");
+});
